c6fbb15ab0903537a82de64188b2f0679504af52
Gayathri575 <gayathriii575@gmail.com>
2025-09-26 23:47:27 +0530

    StudentReportChecker-Task1

---
+++ Operators.js
@@ -0,0 +1,11 @@
+let num = 15;
+if (num %2 === 0){
+    console.log(num + " is even");
+}
+else {
+    console.log(num + " is odd");
+}
+num > 10 ? console.log(num + " is greater than 10") : console.log(num + " is less than or equal to 10");
+if (num > 10 && num % 2 === 1){
+    console.log(num + " is greater than 10 and odd");
+}
